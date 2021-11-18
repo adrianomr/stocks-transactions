73fd4edda1e9ba994bb5ed6c95ff500a19ccde39
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2021-11-18 17:17:05 -0300

    fixing broken tests

--- src/test/java/br/com/adrianorodrigues/stockstransactions/StocksTransactionsApplicationTests.java
+++ src/test/java/br/com/adrianorodrigues/stockstransactions/StocksTransactionsApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.adrianorodrigues.stockstransactions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StocksTransactionsApplicationTests {
 
 	@Test
